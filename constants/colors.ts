e1b51e11c8b78f2b047fe49a505ab47cad62aa74
orianamartina <orianapettinelli04@gmail.com>
2025-06-10 10:57:14 -0300

    design system

---
+++ constants/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  primaryBase: "#01164D",
+  primaryLight: "#345978",
+  secondaryBase: "#4f80ff",
+  secondaryLight: "#a1d0ff",
+  sunYellow: "#ffd233",
+  hotPink: "#fe566a",
+  naruto: "#ea9828",
+  neutral: "#3c4144",
+  ironMan: "#a4afb8",
+  smoke: "#e2e6ea",
+  white: "#ffffff",
+  charcoal: "#3c4144",
+
+  // Feedback
+  error: "#ed1a37",
+  warning: "#f4b510",
+  success: "#05c25c",
+};
---
+++ constants/fonts.ts
@@ -0,0 +1,18 @@
+export const fonts = {
+  inter: {
+    regular: "Inter_400Regular",
+    bold: "Inter-700Bold",
+  },
+  epilogue: {
+    regular: "Epilogue_400Regular",
+    bold: "Epilogue_700Bold",
+  },
+};
+
+export const fontSizes = {
+  h1: 32,
+  h2: 21,
+  body: 16,
+  caption: 14,
+  button: 18,
+};
