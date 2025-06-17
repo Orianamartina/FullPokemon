998d9dbb98beb7d2082695788a5cca172f2b31a3
orianamartina <orianapettinelli04@gmail.com>
2025-06-17 16:19:39 -0300

    chore: remove Async storage from  hooks

--- hooks/pokemon/usePokemonDetail.ts
+++ hooks/pokemon/usePokemonDetail.ts
@@ -1,16 +1,14 @@
 import { fetchPokemonDetail } from "@/api/api";
 import { Pokemon } from "@/types/Pokemon";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useQuery } from "@tanstack/react-query";
 
 const STORAGE_KEY = "pokemonDetail";
 
 export const usePokemonDetail = (id: number) => {
   return useQuery<Pokemon | undefined, Error>({
-    queryKey: ["pokemonDetail"],
+    queryKey: [STORAGE_KEY],
     queryFn: async () => {
       const fetched = await fetchPokemonDetail(id);
-      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(fetched));
       return fetched;
     },
   });
